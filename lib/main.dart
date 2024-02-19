5556f910e83e3e77353aba83612f9a88b4edeac4
ankit <apandey@acrocorp.com>
2024-02-20 01:50:51 +0530

    add

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'Hospital',
           theme: ThemeData(
-            primarySwatch: Colors.blue,
+            primarySwatch: Colors.blueGrey,
           ),
           home: LoginPage(),
         );
